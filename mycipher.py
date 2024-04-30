e89fad62bb3500ed2e01dacc380590ba4bbe88e4
Ibrahim Osman <ibrahim.osman@bison.howard.edu>
2024-04-30 02:59:58 +0000

    Now we can decode any string with any amount of shift (does not include spaces yet)

--- mycipher.py
+++ mycipher.py
@@ -5,8 +5,19 @@ import sys
 # This input_line variable takes the number of shifts we want.
 # The [1] means we take in 1 value
 input_line=sys.argv[1]
-print(input_line)
+
+new=""
 
 for line in sys.stdin:
     line = line.upper()
-    print(line)
+
+# We loop over every character in the string and concatenate the shifted
+# value to the string 'new'.
+    for char in line:
+        if char.isalpha():
+            ascii_value = ord(char) + int(input_line)
+            if ascii_value > ord("Z"):
+                ascii_value -= 26
+            new+=chr(ascii_value)
+    print(new)
+    new=""
